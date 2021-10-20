d8cda669814bf0beaed04dec90bdf19399295982
Tuomas Kivilahti <tuokivilahti@gmail.com>
2021-10-20 10:06:36 +0300

    authentikointi ei pelaa

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ var customerRouter = require("./routes/customer");
 
 var app = express();
 
+const basicAuth = require("express-basic-auth");
+
+app.use(basicAuth({ authorizer: myAuthorizer, authorizeAsync: true }));
+const dotenv = require("dotenv");
+dotenv.config();
+
+function myAuthorizer(username, password, cb) {
+  if (
+    username === process.env.auth_user &&
+    password === process.env.auth_pass
+  ) {
+    return cb(null, true);
+  } else {
+    return cb(null, false);
+  }
+}
 const helmet = require("helmet");
 const cors = require("cors");
 
